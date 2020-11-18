b1f69b183dd12ad665a7f30210a00ed66f3aed3c
Vadim Dorozhkin <poly.paps@gmail.com>
2020-11-18 21:00:04 +0500

    Удалил лишние библиотеки

--- app/src/main/java/ru/polypaps/project_two/MainActivity.kt
+++ app/src/main/java/ru/polypaps/project_two/MainActivity.kt
@@ -3,7 +3,6 @@ package ru.polypaps.project_two
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
@@ -12,12 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button_resulte.setOnClickListener{ checkGay() }
+        button_resulte.setOnClickListener { checkGay() }
     }
 
     @SuppressLint("SetTextI18n")
     fun checkGay() {
-        val result = rnd.nextInt(0,1000).toString()
+        val result = rnd.nextInt(0, 1000).toString()
         textView_resulte.text = "$result.0 %"
     }
 }
