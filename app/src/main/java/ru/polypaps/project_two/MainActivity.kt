a7542acde74dd3e0f81e920734043a71904b3e01
Vadim Dorozhkin <poly.paps@gmail.com>
2020-11-18 20:57:40 +0500

    Гей метр 2.0 готов

--- app/src/main/java/ru/polypaps/project_two/MainActivity.kt
+++ app/src/main/java/ru/polypaps/project_two/MainActivity.kt
@@ -1,11 +1,23 @@
 package ru.polypaps.project_two
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private val rnd = Random
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button_resulte.setOnClickListener{ checkGay() }
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun checkGay() {
+        val result = rnd.nextInt(0,1000).toString()
+        textView_resulte.text = "$result.0 %"
     }
 }
